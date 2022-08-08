0bb9699dd7a1f00afd38aebd4485f26bf7c69228
rodrigopereirac <rodrigo.pereira@pulsus.mobi>
2022-08-07 21:47:12 -0400

    adds cache interceptor impl

---
+++ network/src/main/java/br/com/rcp/hearthstone/interceptors/CachingInterceptorFactory.kt
@@ -0,0 +1,14 @@
+package br.com.rcp.hearthstone.interceptors
+
+import okhttp3.Interceptor
+import okhttp3.logging.HttpLoggingInterceptor
+
+class CachingInterceptorFactory : InterceptorFactory {
+    override fun create(): Interceptor = Interceptor { chain ->
+        val response = chain.proceed(chain.request())
+        val maxAge = 60
+        return@Interceptor response.newBuilder().header("Cache-Control", "public, max-age=$maxAge")
+            .removeHeader("Pragma")
+            .build()
+    }
+}
